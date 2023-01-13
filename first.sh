a9e372a93f4e072e104e73f09d60f08cc1aa4964
crixsz <89030463+crixsz@users.noreply.github.com>
2023-01-13 20:42:41 +0800

    create setup script

---
+++ first.sh
@@ -0,0 +1,30 @@
+#/usr/bin/
+# Get all updates and upgrade from repos
+echo -e "[ Installing updates and upgrades from repos ..]"
+apt-get update >> /dev/null
+apt-get upgrade >> /dev/null
+sleep 10
+# Get all necessary package
+clear
+echo -e "[ Installing necessary packages ..]"
+apt-get install curl >> /dev/null
+sleep 3
+apt-get install neofetch >> /dev/null
+sleep 3
+apt-get install net-tools >> /dev/null
+sleep 3
+apt-get install htop >> /dev/null
+sleep 3 
+apt-get install htop >> /dev/null
+sleep 3 
+curl https://getmic.ro | bash
+clear
+echo -e "[ Adding shorcut ..]"
+sleep 3
+echo "neofetch" >> .profile
+echo "alias ports='netstat -tulpn | grep LISTEN'" >> .profile
+echo -e "[ First time setup completed ! ]"
+sleep 5
+clear
+source .profile 
+
