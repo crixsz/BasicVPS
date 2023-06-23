32864471f11726927b912b346ce7919d8320a947
crixsz <89030463+crixsz@users.noreply.github.com>
2023-06-23 19:17:51 +0800

    checked neofetch

--- first.sh
+++ first.sh
@@ -57,28 +57,24 @@ clear
     clear
     echo -e "[ Adding shorcut ..]"
     sleep 2
-    echo "neofetch" >> .profile
-    touch /usr/local/bin/ports
-    echo "netstat -tulpn | grep LISTEN" > /usr/local/bin/ports
-    chmod +x /usr/local/bin/ports
-    echo -e "[ First time setup completed ! ]"
-    sleep 2
-    clear
-    source .profile
-    # Prompt the user to confirm if they want to reboot
-    read -rp "Are you sure you want to reboot? [y/N] " yn
-
-    # Check if the response is yes or no and act accordingly
-    case "$yn" in
-        [Yy]* )
-            echo -e "[ Rebooting in 5 seconds .. ]"
-            rm -rf first.sh
-            sleep 5
-            reboot
-            ;;
-        * )
-            echo "Aborting reboot."
-            rm -rf first.sh
-            ;;
-    esac
-fi
+    if grep -q "neofetch" /root/.profile; then
+        touch /usr/local/bin/ports
+        echo "netstat -tulpn | grep LISTEN" > /usr/local/bin/ports
+        chmod +x /usr/local/bin/ports
+        echo -e "[ First time setup completed ! ]"
+        sleep 2
+        clear
+        source .profile
+        echo "Done !"
+    else
+        echo "neofetch" >> .profile
+        touch /usr/local/bin/ports
+        echo "netstat -tulpn | grep LISTEN" > /usr/local/bin/ports
+        chmod +x /usr/local/bin/ports
+        echo -e "[ First time setup completed ! ]"
+        sleep 2
+        clear
+        source .profile
+        echo "Done !"
+    fi
+   
