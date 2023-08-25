2322819f90750b7b8cff1c13a85eceb61ad2149a
crixszpikai <xenonzox@gmail.com>
2023-08-25 17:53:10 +0800

    removed code-server and apt upgrade

--- first.sh
+++ first.sh
@@ -3,7 +3,6 @@
 clear
     echo -e "[ Installing updates and upgrades from repos ..]"
     apt-get update -y >> /dev/null
-    apt-get upgrade -y >> /dev/null
     # Get all necessary package
     clear
     echo -e "[ Installing necessary packages ..]"
@@ -15,45 +14,6 @@ clear
     sleep 1
     apt-get install htop -y >> /dev/null
     sleep 1 
-    curl https://getmic.ro | bash
-    sleep 1 
-    curl -fsSL https://code-server.dev/install.sh | sh
-    sleep 2
-    code-server &
-    sleep 2
-    pid=$!
-    kill -INT $pid
-    clear
-    rm -rf .config/code-server
-    mkdir .config/code-server/
-    touch .config/code-server/config.yaml
-    echo "
-    bind-addr: 0.0.0.0:8118
-    auth: password
-    password: zoxxenon
-    cert: false
-    " >> .config/code-server/config.yaml
-    touch /etc/systemd/system/code-server.service
-    echo "
-    [Unit]
-    Description=Code-Server
-    After=network.target
-
-    [Service]
-    Type=simple
-    User=root
-    WorkingDirectory=/root
-    ExecStart=/usr/bin/code-server
-    Restart=always
-    TimeoutSec=30
-
-    [Install]
-    WantedBy=multi-user.target
-
-    " >> /etc/systemd/system/code-server.service
-    systemctl daemon-reload
-    systemctl enable code-server
-    systemctl restart code-server
     clear
     echo -e "[ Adding shorcut ..]"
     sleep 2
